61a2095bece3ecb907d0ff58f835a15465256e4d
Federico Pasquarelli <p.relli_federico@hotmail.it>
2021-02-19 00:25:26 +0100

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
-import Images from "@/views/Images.vue";
-import Backgrounds from "@/views/Backgrounds.vue";
-import Opacity from "@/views/Opacity.vue";
-import About from "@/views/About.vue";
+import Images from "../views/Images.vue";
+import Backgrounds from "../views/Backgrounds.vue";
+import Opacity from "../views/Opacity.vue";
+import About from "../views/About.vue";
 
 export default [
   { path: "/", component: Images },
