6e956831457a68c95dc075dcbb8846a3e83698f5
Federico Pasquarelli <p.relli_federico@hotmail.it>
2021-02-19 01:40:52 +0100

    fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,22 +5,10 @@
     </div>
     <div class="container">
       <a
-        href="https://www.instagram.com/deafhoog/"
-        title="visit my instagram"
-        target="_blank"
-        >Visit My Instagram</a
-      >
-      <a
         href="https://github.com/federicopasquarelli/very-very-lazy-loading"
         title="visit my github"
         target="_blank"
         >Visit My Github</a
-      >
-      <a
-        href="mailto:p.relli_federico@hotmail.it"
-        title="send me an email"
-        target="_blank"
-        >Send me an email</a
       >
     </div>
   </div>
