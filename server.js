afe24480c7433fb18dc0afa85902a4ca79cc08a0
Federico Pasquarelli <p.relli_federico@hotmail.it>
2021-03-04 20:24:56 +0100

    fix

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-const compression = require('compression');
 
 const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
-app.use(compression());
 
 app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
