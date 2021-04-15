76c16aa1e50b3505cd64a2a4da586a19a2c51ac5
Federico Pasquarelli <p.relli_federico@hotmail.it>
2021-04-15 21:03:00 +0200

    fix

--- src/views/Iframe.vue
+++ src/views/Iframe.vue
@@ -2,6 +2,7 @@
   <div class="main-content">
     <div class="title">
       <h1>custom callback lazy loading</h1>
+      <p>Scroll down to see a lazyloaded Youtube iframe</p>
     </div>
     <lazy-iframe></lazy-iframe>
   </div>
@@ -14,7 +15,7 @@ import LazyIframe from "@/components/LazyIframe.vue";
 export default Vue.extend({
   name: "IframeView",
   components: {
-    LazyIframe
-  }
+    LazyIframe,
+  },
 });
 </script>
